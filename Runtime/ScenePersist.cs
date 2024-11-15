880fd4fb0ecfc0d4731a53c1b8b46e0dbcdd2efb
SWRCZK <harleenincube@gmail.com>
2024-11-15 20:21:27 +0100

    init

---
+++ Runtime/ScenePersist.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace Utils
+{
+    public class ScenePersist: MonoBehaviour 
+    {
+        void Awake()
+        {
+            var numScenePersists = FindObjectsOfType<ScenePersist>().Length;
+            if (numScenePersists > 1)
+            {
+                ResetScenePersist();
+            }
+            else
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        public void ResetScenePersist()
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Runtime/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance { get; private set; }
+
+        protected virtual void Awake()
+        {
+            if (Instance != null)
+            {
+                DestroyImmediate(this);
+                return;
+            }
+
+            Instance = GetComponent<T>();
+        }
+    }
+}
